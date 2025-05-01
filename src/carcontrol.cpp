6dcba150c28eb85987ad9321bb8e313663893909
Gabriel Guerreiro <gabriel.eu.br@gmail.com>
2025-05-01 16:52:41 -0300

    added car controls

--- src/carcontrol.cpp
+++ src/carcontrol.cpp
@@ -2,44 +2,53 @@
 
 #include "debug.h"
 
-CarControl::CarControl() {}
+CarControl::CarControl() :
+    motor_pin_a(26), motor_pin_b(27), front_wheels_servo()
+{}
 
 bool CarControl::setup() {
+    pinMode(this->motor_pin_a, OUTPUT);
+    pinMode(this->motor_pin_b, OUTPUT);
+
+    this->front_wheels_servo.attach(13);
     return true;
 }
 
 void CarControl::go_forward() {
     DEBUGLN("Frente");
 
-    // TODO Ir para frente
+    digitalWrite(this->motor_pin_a, LOW);
+    digitalWrite(this->motor_pin_b, HIGH);
 }
 
 void CarControl::go_backwards() {
     DEBUGLN("Tras");
 
-    // TODO Ir para trás
+    digitalWrite(this->motor_pin_a, HIGH);
+    digitalWrite(this->motor_pin_b, LOW);
 }
 
 void CarControl::stop() {
     DEBUGLN("Parada");
 
-    // TODO Parar
+    digitalWrite(this->motor_pin_a, LOW);
+    digitalWrite(this->motor_pin_b, LOW);
 }
 
 void CarControl::point_left() {
     DEBUGLN("Esquerda");
 
-    // TODO Apontar para a esquerda
+    this->front_wheels_servo.write(0);
 }
 
 void CarControl::point_ahead() {
     DEBUGLN("Centro");
 
-    // TODO Apontar para frente
+    this->front_wheels_servo.write(90);
 }
 
 void CarControl::point_right() {
     DEBUGLN("Direita");
 
-    // TODO Apontar para a direita
+    this->front_wheels_servo.write(180);
 }
--- src/carcontrol.h
+++ src/carcontrol.h
@@ -1,17 +1,46 @@
 #include <cstddef>
 
+#include <ESP32Servo.h>
+
 class CarControl {
     public:
         CarControl();
 
         bool setup();
 
+        /**
+         * Instructs the car to go ahead. It will keep going forward until `go_backwards`or `stop` is called.
+         */
         void go_forward();
+
+        /**
+         * Instructs the car to go backwards. It will keep going backwards until `go_forward` or `stop` is called.
+         */
         void go_backwards();
+
+        /**
+         * Turn off tyre traction.
+         */
         void stop();
+
+        /**
+         * Make the front wheels turn left
+         */
         void point_left();
+
+        /**
+         * Make the front wheels straight
+         */
         void point_ahead();
+
+        /**
+         * Make the front wheels turn right
+         */
         void point_right();
 
         // TODO Funções para controlar os LEDs
+    
+    private:
+        uint8_t motor_pin_a, motor_pin_b;
+        Servo front_wheels_servo;
 };
