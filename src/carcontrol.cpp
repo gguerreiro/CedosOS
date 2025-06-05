c04cd339f8a7602613b241d4e90c88d6f5b7bd6b
EduSTC <edustclair@gmail.com>
2025-06-05 07:45:03 -0300

    added bimotor branch

--- src/carcontrol.cpp
+++ src/carcontrol.cpp
@@ -3,29 +3,34 @@
 #include "debug.h"
 
 CarControl::CarControl() :
-    motor_pin_a(22), motor_pin_b(23), front_wheels_servo()
+    motor_pin_a(22), motor_pin_b(23), motor_2_pin_a(24), motor_2_pin_b(25)
 {}
 
 bool CarControl::setup() {
     pinMode(this->motor_pin_a, OUTPUT);
     pinMode(this->motor_pin_b, OUTPUT);
+    pinMode(this->motor_2_pin_a, OUTPUT);
+    pinMode(this->motor_2_pin_b, OUTPUT);
 
-    this->front_wheels_servo.attach(13);
     return true;
 }
 
 void CarControl::go_forward() {
     DEBUGLN("Frente");
 
-    digitalWrite(this->motor_pin_a, LOW);
-    digitalWrite(this->motor_pin_b, HIGH);
+    digitalWrite(this->motor_pin_a, HIGH);
+    digitalWrite(this->motor_pin_b, LOW);
+    digitalWrite(this->motor_2_pin_a, LOW);
+    digitalWrite(this->motor_2_pin_b, HIGH);
 }
 
 void CarControl::go_backwards() {
     DEBUGLN("Tras");
 
-    digitalWrite(this->motor_pin_a, HIGH);
-    digitalWrite(this->motor_pin_b, LOW);
+    digitalWrite(this->motor_pin_a, LOW);
+    digitalWrite(this->motor_pin_b, HIGH);
+    digitalWrite(this->motor_2_pin_a, HIGH);
+    digitalWrite(this->motor_2_pin_b, LOW);
 }
 
 void CarControl::stop() {
@@ -33,22 +38,29 @@ void CarControl::stop() {
 
     digitalWrite(this->motor_pin_a, LOW);
     digitalWrite(this->motor_pin_b, LOW);
+    digitalWrite(this->motor_2_pin_a, LOW);
+    digitalWrite(this->motor_2_pin_b, LOW);
+    
 }
 
 void CarControl::point_left() {
     DEBUGLN("Esquerda");
-
-    this->front_wheels_servo.write(0);
+    digitalWrite(this->motor_pin_a, HIGH);
+    digitalWrite(this->motor_pin_b, LOW);
+    digitalWrite(this->motor_2_pin_a, LOW);
+    digitalWrite(this->motor_2_pin_b, LOW);
+    
 }
 
 void CarControl::point_ahead() {
     DEBUGLN("Centro");
-
-    this->front_wheels_servo.write(90);
 }
 
 void CarControl::point_right() {
     DEBUGLN("Direita");
+    digitalWrite(this->motor_pin_a, LOW);
+    digitalWrite(this->motor_pin_b, LOW);
+    digitalWrite(this->motor_2_pin_a, LOW);
+    digitalWrite(this->motor_2_pin_b, HIGH);
 
-    this->front_wheels_servo.write(180);
 }
